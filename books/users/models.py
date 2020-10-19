a23d76c15e0e7fbb1185d754c5f340046ae62131
Jeffery Roche <jkeauctions@gmail.com>
2020-10-19 14:58:30 -0400

    Commit of chapter 3

--- books/users/models.py
+++ books/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import Models
+from django.db import models
 
-class CustomUSer(AbstractUser):
+
+class CustomUser(AbstractUser):
     pass
